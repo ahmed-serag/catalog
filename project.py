9044e3d9da86dff93b65082dba409b544ab814a7
ahmed-serag <ahmode2003@gmail.com>
2017-07-22 20:22:23 +0200

    Update project.py

--- project.py
+++ project.py
@@ -317,7 +317,4 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=80)
+
